40063e6426cbb6f17bbe5aceb657ed9b1238fcee
gabbigum <gumata.gumata@gmail.com>
2021-04-21 15:29:25 +0300

    add routing + in memory db

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, pipe } from 'rxjs';
 import { MessageService } from './message.service';
-import { Hero }  from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -10,11 +11,30 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
+     .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+     );
+  }
+
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error:any): Observable<T> => {
+      console.error(error);
+
+      this.log('${operation} failed: ${error.message}');
+
+      return of(result as T);
+    }
+  }
+
+  log(message: string) {
+    this.messageService.add(message);
   }
 }
